57e5bfc7ea8eb5bea10e48e49363424bcf5adc9d
Ayan Mondal <am9330937@gmail.com>
2026-06-21 20:47:27 +0530

    Refactor weather data fetching to use geolocation
    Updated getData function to accept latitude and longitude instead of city name. Added functionality to handle geolocation.

--- weatherapk/weather.js
+++ weatherapk/weather.js
@@ -1,13 +1,13 @@
 const button = document.getElementById("searchbtn");
 const city = document.getElementById("cityinput");
-
+const Tepo = document.getElementById('clickButton');
 const cityName = document.getElementById("cityname");
 const temp = document.getElementById("temperature");
 const localTime = document.getElementById("localtime");
 
-async function getData(cityName) {
+async function getData(lat, long) {
     const promise = await fetch(
-   (`http://api.weatherapi.com/v1/current.json?key=16c0bd25db5a4c009d8125432261806&q=${cityName}&aqi=yes`)
+   (`http://api.weatherapi.com/v1/current.json?key=16c0bd25db5a4c009d8125432261806&q=${lat},${long}&aqi=yes`)
 )
 return await promise.json();
 }
@@ -20,4 +20,11 @@ button.addEventListener("click", async () => {
   temp.innerText = ` ${result.current.temp_c}°C`;
   localTime.innerText = ` ${result.location.localtime}`;
 })
+async function getlocation(position){
+const result =    await getData(position.coords.latitude, position.coords.longitude)
+console.log(result)
+}
+function failToGetLocation (){
+    console.log("user not allowig to access location")
+}
 
